5d140914b7a8a5d00deb94af46e2e68a65abc86c
Jolene Kearse <pharaohnutz@gmail.com>
2022-12-01 15:20:04 -0500

    feat: got functionality up

--- ts-todo/src/main.ts
+++ ts-todo/src/main.ts
@@ -10,15 +10,32 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector("#new-task-form") as HTMLButtonElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
 
 function addListItem(task: Task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks()
+  })
   checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
 }
 
 form?.addEventListener("submit", (e) => {
@@ -32,7 +49,9 @@ form?.addEventListener("submit", (e) => {
     completed: false,
     createdAt: new Date()
   };
-
+  tasks.push(newTask);
+  saveTasks();
   addListItem(newTask);
+  input.value = "";
 })
 
